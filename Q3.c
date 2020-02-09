23798e4816707025dd73694a2de2620efa3f080f
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-02-09 20:17:21 +0500

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -11,7 +11,7 @@ int main(){
 	int flag = 0;
 
 	//memset(buff,'\0',sizeof(buff));
-    //memset(new,'\0',sizeof(new));
+    
 	rewind(fp1);
 	
 
@@ -19,18 +19,20 @@ int main(){
 		for(int i=0; i< sizeof(buff); i++){
 			if ( buff[i] == "a" || buff[i] == "e" || buff[i] == "i" || buff[i] == "o" || buff[i] == "u" || 
 				buff[i] == "A" || buff[i] == "E" || buff[i] == "I" || buff[i] == "O" || buff[i] == "U"){
-				flag == 1;
+				flag = 1;
 			}
-			if(flag == 1){
-				for(int j = 0; j< sizeof(buff); i++ ){
+		}
+		if(flag == 0){
+				for(int j = 0; j< sizeof(buff); j++ ){
 					new[j]=buff[sizeof(buff)-1];
 				}
-				break;
-			}
+				printf("%s\n", new);
+				memset(new,'\0',sizeof(new));
 		}
 	}
 		
-	
+	printf("%s\n", buff);
+	printf("%s\n", new);
 	fclose(fp1);
 	//fclose(fp2);
 
