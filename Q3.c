c7c73607f3335c684f97af1dcac659a8caafbf05
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-02-09 21:45:53 +0500

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -4,40 +4,48 @@
 int main(){
 
 	
-	FILE *fp1 = fopen ("q3_a.txt","a+");
-	FILE *fp2 = fopen ("output","a+");
+	FILE *fp1 = fopen ("q3_a.txt","r");
+	FILE *fp2 = fopen ("new","a+");
 
-	char buff[100];
-	char new [100];
+	char buff[10];
+	char new [10];
 	int flag = 0;
-    
+	memset(buff,'\0',sizeof(buff));
+    memset(new,'\0',sizeof(new));
 	rewind(fp1);
-	
+	int i;
 
 	while(fscanf(fp1, "%s", buff) !=EOF){
-		for(int i=0; i< sizeof(buff); i++){
-			if ( strcmp(&buff[i],"a") || strcmp(&buff[i],"e") || strcmp(&buff[i],"i") || strcmp(&buff[i],"o") || strcmp(&buff[i],"u") || 
-				strcmp(&buff[i],"A") || strcmp(&buff[i],"E") || strcmp(&buff[i],"I") || strcmp(&buff[i],"O") || strcmp(&buff[i],"U")){
+		for(i=0; buff[i]!='\0'; i++)
+		{
+			//printf("Strcmp: %d ", strcmp(&buff[i],"i"));
+			//printf("The character is =%c \n",buff[i]);
+			// if ( strcmp(&buff[i],"a") == 0 || strcmp(&buff[i],"e") == 0 || strcmp(&buff[i],"i") == 0 || strcmp(&buff[i],"o") == 0 || strcmp(&buff[i],"u") == 0 || 
+			// 	 strcmp(&buff[i],"A") == 0 || strcmp(&buff[i],"E") == 0 || strcmp(&buff[i],"I") == 0 || strcmp(&buff[i],"O") == 0 || strcmp(&buff[i],"U") == 0){
+		if ( buff[i] == 'a' || buff[i] == 'e' || buff[i] == 'i' || buff[i] == 'o' || buff[i] == 'u')
+		{
 				flag = 1;
+			printf("The flag = %d for %c \n", flag, buff[i]);
 			}
 		}
+
 		if(flag == 0){
-			for(int j = 0; j< sizeof(buff); j++ ){
-				new[j]=buff[sizeof(buff)-j];
+			int i = strlen(buff) - 1;
+			for(int j = 0; j < strlen(buff); j++ ){
+				new[j] = buff[i];
+				i--;
 			}
-			fputs(new, output);
-			// printf("%s\n", new);
-			memset(new,'\0',sizeof(new));
+			i = 0;
+			fputs(new, fp2);			
 		}
 		else if(flag == 1){
-			fputs(buff, output);
-			//printf("%s\n", buff);
-			memset(buff,'\0',sizeof(buff));
+			fputs(buff, fp2);			
+			
 		}
+		memset(new,'\0',sizeof(new));
+		memset(buff,'\0',sizeof(buff));
+		flag = 0;
 	}
-		
-	printf("%s\n", buff);
-	printf("%s\n", new);
 	fclose(fp1);
 	fclose(fp2);
 
