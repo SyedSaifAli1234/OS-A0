e76d091f0807f68de15ef6344a71fdba2101bf19
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-02-09 20:48:22 +0500

    Update Q3.c

--- Q3.c
+++ Q3.c
@@ -3,38 +3,43 @@
 
 int main(){
 
+	
 	FILE *fp1 = fopen ("q3_a.txt","a+");
-	//FILE *fp2 = fopen ("output","a+");
+	FILE *fp2 = fopen ("output","a+");
 
 	char buff[100];
 	char new [100];
 	int flag = 0;
-
-	//memset(buff,'\0',sizeof(buff));
     
 	rewind(fp1);
 	
 
 	while(fscanf(fp1, "%s", buff) !=EOF){
 		for(int i=0; i< sizeof(buff); i++){
-			if ( buff[i] == "a" || buff[i] == "e" || buff[i] == "i" || buff[i] == "o" || buff[i] == "u" || 
-				buff[i] == "A" || buff[i] == "E" || buff[i] == "I" || buff[i] == "O" || buff[i] == "U"){
+			if ( strcmp(&buff[i],"a") || strcmp(&buff[i],"e") || strcmp(&buff[i],"i") || strcmp(&buff[i],"o") || strcmp(&buff[i],"u") || 
+				strcmp(&buff[i],"A") || strcmp(&buff[i],"E") || strcmp(&buff[i],"I") || strcmp(&buff[i],"O") || strcmp(&buff[i],"U")){
 				flag = 1;
 			}
 		}
 		if(flag == 0){
-				for(int j = 0; j< sizeof(buff); j++ ){
-					new[j]=buff[sizeof(buff)-1];
-				}
-				printf("%s\n", new);
-				memset(new,'\0',sizeof(new));
+			for(int j = 0; j< sizeof(buff); j++ ){
+				new[j]=buff[sizeof(buff)-j];
+			}
+			fputs(new, output);
+			// printf("%s\n", new);
+			memset(new,'\0',sizeof(new));
+		}
+		else if(flag == 1){
+			fputs(buff, output);
+			//printf("%s\n", buff);
+			memset(buff,'\0',sizeof(buff));
 		}
 	}
 		
 	printf("%s\n", buff);
 	printf("%s\n", new);
 	fclose(fp1);
-	//fclose(fp2);
+	fclose(fp2);
 
 	return 0;
 }
