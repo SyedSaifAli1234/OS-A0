f4839f9f33a3f13b310d23a4451180a624a03a0b
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-02-11 23:21:59 +0500

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -3,7 +3,23 @@
 
 int main(int argc, char* arg[]){
 
-	FILE *fp1 = fopen ("Input.txt","r");
+	FILE *fp1;
+
+	if(*arg[1] == 'a'){
+		fp1 = fopen ("q1_a.txt","r");
+	}
+	else if(*arg[1] == 'b'){
+		fp1 = fopen ("q1_b.txt","r");
+	}
+	else if(*arg[1] == 'c'){
+		fp1 = fopen ("q1_c.txt","r");
+	}
+	else{
+		printf("Invalid arguement \n");
+		exit(0);
+	}
+
+	//FILE *fp1 = fopen ("Input.txt","r");
 	int n=0;
 
 	if (fp1 == 0)
@@ -12,16 +28,16 @@ int main(int argc, char* arg[]){
 		return 1;
 	}
 
-    char c = fgetc(mf);
+    char c = fgetc(fp1);
 
 	while ( c != EOF )
 	{
 		if(c >= '0' && c <= '9'){
 			n=n+1;
-			c = fgetc(mf);
+			c = fgetc(fp1);
 		}
 		else{
-			c = fgetc(mf);
+			c = fgetc(fp1);
 		}
 
 	}
