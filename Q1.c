47fd714e7d678df33a26588c21235f0733164898
SyedSaifAli1234 <52702293+SyedSaifAli1234@users.noreply.github.com>
2020-02-13 04:48:52 +0500

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -3,28 +3,28 @@
 
 int main(int argc, char* arg[]){
 
-	FILE *fp1;
+	FILE *fp1 = fopen(arg[1],"r");
 
-	if(*arg[1] == 'a'){
-		fp1 = fopen ("q1_a.txt","r");
-	}
-	else if(*arg[1] == 'b'){
-		fp1 = fopen ("q1_b.txt","r");
-	}
-	else if(*arg[1] == 'c'){
-		fp1 = fopen ("q1_c.txt","r");
-	}
-	else{
-		printf("Invalid arguement \n");
-		exit(0);
-	}
+	// if(*arg[1] == 'a'){
+	// 	fp1 = fopen ("q1_a.txt","r");
+	// }
+	// else if(*arg[1] == 'b'){
+	// 	fp1 = fopen ("q1_b.txt","r");
+	// }
+	// else if(*arg[1] == 'c'){
+	// 	fp1 = fopen ("q1_c.txt","r");
+	// }
+	// else{
+	// 	printf("Invalid arguement \n");
+	// 	exit(0);
+	// }
 
 	//FILE *fp1 = fopen ("Input.txt","r");
 	int n=0;
 
 	if (fp1 == 0)
 	{
-		printf ("Error Opening File");
+		printf ("Error Opening File \n");
 		return 1;
 	}
 
